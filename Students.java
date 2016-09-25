825461123a49d55859a4af7d60e8ad3a9b14062f
scarletknight11 <sanjitpsingh@yahoo.com>
2016-09-25 02:00:21 -0400

    Create Students.java

---
+++ Students.java
@@ -0,0 +1,29 @@
+package com.example.medbox;
+
+
+public class Students {
+
+	private int id;
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
