62b45540e575fe786d54f6a8d778b1d189fe35fe
Sanjit Singh <sanjitpsingh@yahoo.com>
2017-05-26 15:59:34 -0400

    Add files via upload

---
+++ BacktoTitle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BacktoTitle : MonoBehaviour {
+
+	int timer = 0;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			SceneManager.LoadScene ("Merged Scene");
+		}
+	}
+}
